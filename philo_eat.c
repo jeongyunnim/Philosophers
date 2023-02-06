c8bf7fc85042a25e4d6d1346fbcd5f7a43fed2f8
jeongyunnim <jeongyun7521@gmail.com>
2023-02-06 22:00:30 +0900

    bug: 컴파일 오류 발견

--- philo_eat.c
+++ philo_eat.c
@@ -6,7 +6,7 @@
 /*   By: jeseo <jeseo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 15:50:43 by jeseo             #+#    #+#             */
-/*   Updated: 2023/02/06 21:44:14 by jeseo            ###   ########.fr       */
+/*   Updated: 2023/02/06 22:00:07 by jeseo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,12 +54,20 @@ int	pick_up_forks_odd(t_philo *shared, int num, int left_fork, int right_fork)
 	return (0);
 }
 
-void	put_down_forks(t_philo *shared, int left_fork, int right_fork)
+void	put_down_forks_even(t_philo *shared, int left_fork, int right_fork)
 {
-	shared->fork[right_fork] = 0;
 	shared->fork[left_fork] = 0;
+	shared->fork[right_fork] = 0;
 	pthread_mutex_unlock(&shared->fork_mutex[left_fork]);
 	pthread_mutex_unlock(&shared->fork_mutex[right_fork]);
+}
+
+void	put_down_forks_odd(t_philo *shared, int left_fork, int right_fork)
+{
+	shared->fork[left_fork] = 0;
+	shared->fork[right_fork] = 0;
+	pthread_mutex_unlock(&shared->fork_mutex[right_fork]);
+	pthread_mutex_unlock(&shared->fork_mutex[left_fork]);
 }
 
 int	eating_spaghetti(t_philo *shared, int num, int left_fork, int right_fork)
@@ -83,10 +91,13 @@ int	eating_spaghetti(t_philo *shared, int num, int left_fork, int right_fork)
 	shared->last_eat[num - 1] = get_time();
 	pthread_mutex_unlock(&shared->last_eat_mutex[num]);
 	pthread_mutex_lock(&shared->mutexes[EATCNT_M]);
-	printf("\t\t\t 먹었으면 뭐라도 해\n");
 	shared->eat_cnt[num - 1] += 1;
 	pthread_mutex_unlock(&shared->mutexes[EATCNT_M]);
 	split_usleep(shared->conditions->time_to_eat);
-	put_down_forks(shared, left_fork, right_fork);
+	if (num % 2 == 1)
+		put_down_forks_even(shared, left_fork, right_fork);
+	else
+		put_down_forks_odd(shared, left_fork, right_fork);
+	//put_down_forks(shared, left_fork, right_fork);
 	return (0);
 }
